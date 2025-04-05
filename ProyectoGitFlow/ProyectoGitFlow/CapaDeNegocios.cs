3404df8fe84529f8fc785ec530825784c914c7e9
Toncoas <angelluisdelasnuecesramirez@gmail.com>
2025-04-04 20:35:16 -0400

    creacion de las capas y funcionalidad del boton guardar

---
+++ ProyectoGitFlow/ProyectoGitFlow/CapaDeNegocios.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ProyectoGitFlow.CapaDeDatos;
+using static ProyectoGitFlow.CapaDeEntidades;
+using static ProyectoGitFlow.Form1;
+
+
+
+namespace ProyectoGitFlow
+{
+    internal class CapaDeNegocios
+    {
+        private readonly CapaDeDatos capaDeDatos = new CapaDeDatos();
+
+        public void RegistrarAutobus(CapaDeEntidades.Autobus autobus)
+        {
+            if (string.IsNullOrWhiteSpace(autobus.Marca) || string.IsNullOrWhiteSpace(autobus.Placa))
+            {
+                MessageBox.Show("La marca y la placa son obligatorias.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            capaDeDatos.RegistrarAutobus(autobus);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
